cf288d3d995eedfe8b34a882ce344b2472ac385c
minhcong <83624415+dmcong@users.noreply.github.com>
2023-05-30 23:28:40 +0700

    fix config

--- frontend/src/data/constant.js
+++ frontend/src/data/constant.js
@@ -3,13 +3,13 @@ class Constant {
     this.budgetRange = {
       min: 100000,
       max: 2000000,
-      step: 100000
-    }
+      step: 100000,
+    };
     this.timeRange = {
       min: 1800,
       max: 36000,
-      step: 1800
-    }
+      step: 1800,
+    };
     this.TIMEOUT_FEEDBACK = 2500;
     this.GOOGLE_MAPS_BASE_URL = "https://www.google.com/maps/place/";
     this.CAROUSEL_PARENT = 3;
@@ -33,9 +33,9 @@ class Constant {
     this.TITLE_MAX = 50;
     this.DESCRIPTION_MAX = 1500;
     this.IMAGES_UPLOAD_MAX = 12;
-    this.BACKEND_BASE_URL = "http://localhost:5000/api/v1";
+    this.BACKEND_BASE_URL = "https://thanhthai-webdev2023-be.onrender.com/api/v1";
     this.PROVINCE_API_BASE_URL = "https://provinces.open-api.vn/api";
-  };
-};
+  }
+}
 
 export default new Constant();
